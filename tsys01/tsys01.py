dd3868c38b05448701e2dfe55dbc019a3208888f
Levi Cai <lcai@whoi.edu>
2020-11-12 03:20:33 +0000

    Fixed print statement issue with bus outside quotes

--- tsys01/tsys01.py
+++ tsys01/tsys01.py
@@ -28,7 +28,7 @@ class TSYS01(object):
         try:
             self._bus = smbus.SMBus(bus)
         except:
-            print("Bus %d is not available.") % bus
+            print("Bus %d is not available."%bus)
             print("Available busses are listed as /dev/i2c*")
             self._bus = None
         
