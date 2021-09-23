c92a74f04894d2ec8503e2a40b2ef53b9339eb04
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 18:16:23 +1000

    test-tsys01: simplify with abstracted logging

--- test-tsys01.py
+++ test-tsys01.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 
-import argparse
 from tsys01 import TSYS01
-from pathlib import Path
-import llog
-import time
+from llog import LLogWriter
 
 device = "tsys01"
-defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
-
-parser = argparse.ArgumentParser(description=f'{device} test')
-parser.add_argument('--output', action='store', type=str, default=None)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--frequency', action='store', type=int, default=1)
+parser = LLogWriter.create_default_parser(__file__, device, default_frequency=1)
 args = parser.parse_args()
 
 
@@ -20,17 +12,12 @@ with llog.LLogWriter(args.meta, args.output) as log:
     sensor = TSYS01()
     
     if not sensor.init():
-        log.log(llog.LLOG_ERROR, "Error initializing sensor")
+        log.log_error("Error initializing sensor")
         exit(1)
 
-    while True:
-        try:
-            if not sensor.read():
-                log.log(llog.LLOG_ERROR, "Error reading sensor")
-            else:
-                log.log(llog.LLOG_DATA, f"{sensor.temperature()}")
-        except Exception as e:
-            log.log(llog.LLOG_ERROR, e)
-
-        if args.frequency:
-            time.sleep(1.0/args.frequency)
+    def data_getter():
+        if not sensor.read():
+            raise Exception("Error reading sensor")
+        return f"{sensor.temperature()}"
+    
+    log.log_data_loop(data_getter, parser_args=args)
